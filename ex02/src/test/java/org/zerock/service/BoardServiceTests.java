a788fe6aa03c19c2f354e6024a21e302a0fdbd0a
Administrator <Administrator@User -2022TSFVF>
2022-08-30 17:00:15 +0900

    bootstrap 적용(SB Admin2)

--- ex02/src/test/java/org/zerock/service/BoardServiceTests.java
+++ ex02/src/test/java/org/zerock/service/BoardServiceTests.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -23,5 +24,43 @@ public class BoardServiceTests {
 	public void testExist() {
 		log.info(service);
 		assertNotNull(service);
+	}
+	@Test
+	public void testRegister() {
+		BoardVO board = new BoardVO();
+		board.setTitle("���� �ۼ��ϴ� ��");
+		board.setContent("���� �ۼ��ϴ� ����");
+		board.setWriter("newbie");
+
+		service.register(board);
+
+		log.info("������ �Խù��� ��ȣ: " + board.getBno());
+	}
+
+	@Test
+	public void testGetList() {
+		service.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testGet() {
+		log.info(service.get(1L));
+	}
+	
+	@Test
+	public void testDelete() {
+		log.info("REMOVE RESULT: " + service.remove(2L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = service.get(1L);
+		
+		if(board == null) {
+			return;
+		}
+		
+		board.setTitle("���� �����մϴ�.");
+		log.info("MODIFY RESULT : " + service.modify(board));
 	}
 }
